6b053bb28b11748f5f2ea46324c295cf3e9d957a
2559055 <mohamed.hussein@student.moringaschool.com>
2023-11-17 12:58:47 -0800

    React Fetch CRUD Codealong

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
 
-import React, { useState, useEffect } from 'react';
+
+// src/App.js
+import React from 'react';
+import './App.css';
+import ShoppingList from './Components/ShoppingList';
+import ItemForm from './components/ItemForm';
+import Item from './components/Item';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-
-  useEffect(() => {
-    document.title = text;
-  }, [text]);
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      // Using the current count value from the state
-      setCount((prevCount) => prevCount + 1);
-    }, 5000);
-
-    // Cleanup function to clear the timeout when the component unmounts
-    return () => clearTimeout(timeoutId);
-  }, []); // Empty dependency array means this effect runs once, similar to componentDidMount
-
-  console.log("Component rendering");
-
   return (
-    <div>
-      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
-        I've been clicked {count} times
-      </button>
-      <input
-        type="text"
-        placeholder="Type away..."
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
+    <div className="App">
+      <h1>Shopping List App</h1>
+      <ShoppingList />
     </div>
   );
 }
